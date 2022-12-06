e8de122eecfe5789694bd396f5bd19eb66caab02
Nathan Barloy <nbarloy@gmail.com>
2022-12-06 12:55:41 +0100

    minor change day 6

--- day 6/code.py
+++ day 6/code.py
@@ -1,7 +1,10 @@
 with open("input.txt") as f:
     signal = f.read()
     
-    for i in range(14, len(signal)+1):
-        if len(set(signal[i-14:i]))==14:
+    #n = 4
+    n = 14
+
+    for i in range(n, len(signal)+1):
+        if len(set(signal[i-n:i]))==n:
             print(i)
             break
